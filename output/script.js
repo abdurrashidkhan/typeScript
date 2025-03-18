92df05d492d4f139e0ffd51b548bd28f28d7cb9e
abdurrashidkhan <rashidkhan.destop@gmail.com>
2025-03-18 13:56:38 +0600

    update here

--- output/script.js
+++ output/script.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("Hello, Bangladesh is my country!");
+const text = "Hello, Bangladesh is my country. !";
+console.log(text);
+const country = () => {
+    const regex = /Bangladesh/i;
+    const match = text.match(regex);
+    console.log(match);
+};
+country();
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,9 @@
-console.log("Hello, Bangladesh is my country!");
+const text = "Hello, Bangladesh is my country. !";
+console.log(text);
+
+const country = () => {
+  const regex = /Bangladesh/i;
+  const match = text.match(regex);
+  console.log(match);
+};
+country();
